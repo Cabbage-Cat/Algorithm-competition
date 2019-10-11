34bc3c7d41092160c4a3d75d29f69f27d989a189
SsorryQaQ <ssorryqaq@gmail.com>
2019-10-11 22:49:50 +0800

    P1091

--- Luogu/P1091.cpp
+++ Luogu/P1091.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+const int maxn = 100 + 10;
+int d[maxn];
+int f[2][maxn];
+int n;
 int main(){
+  cin >> n; for (int i=1;i<=n;i++) scanf("%d",d+i);
+  for (int i=1;i<=n;i++)
+    for (int j=0;j<i;j++)
+      if (d[i]>d[j]) f[0][i] = max(f[0][i],f[0][j]+1);
+  for (int i=n;i>0;i--)
+    for (int j=n+1;j>i;j--)
+      if (d[i]>d[j]) f[1][i] = max(f[1][i],f[1][j]+1);
+  int res = 0;
+  for (int i=1;i<=n;i++) res = max(res,f[0][i]+f[1][i]-1);
+  cout << n-res;
   return 0;
 }
