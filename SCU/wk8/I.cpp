9c17cb0750509b5854e29e835a5f3880520315d0
SsorryQaQ <ssorryqaq@gmail.com>
2019-10-28 23:38:17 +0800

    SCU WK8 IJ

--- SCU/wk8/I.cpp
+++ SCU/wk8/I.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+const int maxn = 1000000 + 10;
+int d[maxn];
 int main(){
+  int n; cin >> n; int res = 0; int x;
+  for (int i=0;i<n;i++){
+    scanf("%d",&x);
+    if (x%2) { res=1; break; }
+  }
+  if (res) cout << "First";
+  else cout << "Second";
+  
   return 0;
 }
--- SCU/wk8/J.cpp
+++ SCU/wk8/J.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+const int maxn = 100 + 10;
+int l[maxn];
 int main(){
+  int a,b,c,d; cin >> a >> b >> c >> d;
+  int res=-1;
+  int flag = 1;
+  if (b==d || (b-d>0&&(b-d)%c==0) ) { res=b; flag = 0; }
+  if ((d-b>0&&(d-b)&a==0)) { res = d; flag =  0;}
+  if (flag){
+    for (int k2=0;!l[k2];k2++){
+      int r = d-b+k2*c;
+      if (r>=0)
+        if (r%a==0) { flag = 0; res = r+b; break; }
+    }
+  }
+  if (flag) cout << -1;
+  else cout << res;
+  
   return 0;
 }
